6b3bc8885ea5c34d054bf64a54041f156eee1263
Vrigz Alejo <nuedu2012@gmail.com>
2016-01-02 17:33:03 +0800

    tutorial14

--- public/scripts/tutorial14.js
+++ public/scripts/tutorial14.js
@@ -2,27 +2,31 @@
  * Created by vrigzlinuxmint13 on 12/26/15.
  */
 var CommentBox = React.createClass( {
-	getInitialState: function() {
-		return {data: []};
-	},
-	componentDidMount: function() {
-		$.ajax({
+	loadCommentsFromServer: function() {
+		$.ajax( {
 			url: this.props.url,
 			dataType: 'json',
 			cache: false,
-			success: function(data) {
-				this.setState({data: data});
-			}.bind(this),
-			error: function(xhr, status, err) {
-				console.error(this.props.url, status, err.toString());
-			}.bind(this)
-		});
+			success: function( data ) {
+				this.setState( { data: data } );
+			}.bind( this ),
+			error: function( xhr, status, err ) {
+				console.error( this.props.url, status, err.toString() );
+			}.bind( this )
+		} );
+	},
+	getInitialState: function() {
+		return { data: [] };
+	},
+	componentDidMount: function() {
+		this.loadCommentsFromServer();
+		setInterval( this.loadCommentsFromServer, this.props.pollInterval );
 	},
 	render: function() {
 		return (
 			<div className="commentBox">
 				<h1>Comments</h1>
-				<CommentList data={this.state.data} />
+				<CommentList data={this.state.data}/>
 				<CommentForm />
 			</div>
 		);
@@ -30,13 +34,13 @@ var CommentBox = React.createClass( {
 } );
 var CommentList = React.createClass( {
 	render: function() {
-		var commentNodes = this.props.data.map(function(comment) {
+		var commentNodes = this.props.data.map( function( comment ) {
 			return (
 				<Comment author={comment.author} key={comment.id}>
 					{comment.text}
 				</Comment>
 			);
-		});
+		} );
 		return (
 			<div className="commentList">
 				{commentNodes}
@@ -69,11 +73,7 @@ var Comment = React.createClass( {
 		);
 	}
 } );
-var data = [
-	{id: 1, author: "Pete Hunt", text: "This is one comment"},
-	{id: 2, author: "Jordan Walke", text: "This is *another* comment"}
-];
 ReactDOM.render(
-	<CommentBox url="/api/comments" />,
+	<CommentBox url="/api/comments" pollInterval={2000}/>,
 	document.getElementById( 'content' )
 );
