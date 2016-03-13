2c1e156b1334ff8b0244f356496e07a2c4402beb
Vrigz Alejo <nuedu2012@gmail.com>
2016-03-13 13:10:46 +0800

    Hot Module Reloading | React + Redux + Webpack (Part 4) - https://www.youtube.com/watch?v=DNM46IYrUcU&list=PLQDnxXqV213JJFtDaG0aE9vqvp6Wm7nBg&index=4

--- components/App.js
+++ components/App.js
@@ -1,1 +1,11 @@
+import React, { Component } from 'react'
 
+class App extends Component {
+
+	render() {
+		return <div>This is a definitely a hot (module reloading) React app now!</div>
+	}
+
+}
+
+export default App
